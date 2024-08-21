0c199b13ccbe75a6d05e99f05e71e8df25b53735
lalit-rg <rg.lalitkumar26@gmail.com>
2024-08-21 19:05:03 +0530

    utils added

---
+++ demo.py
@@ -0,0 +1,8 @@
+from  us_visa.logger import logging
+logging.info("welcome to our custom log")
+from us_visa.exception import USvisaException
+import sys
+try :
+    2/0
+except Exception as e:
+    raise USvisaException(e,sys)
